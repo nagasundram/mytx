4f5a2273cb5b12f2ae5396449b2461143085f501
nagasundaram-wk <nagasundaram.ilango@wolterskluwer.com>
2018-07-23 14:04:09 +0530

    Read More Details feature

--- popup.js
+++ popup.js
@@ -11,7 +11,8 @@ function write_action(amt, cat, subCat, source, comments,location) {
       document.getElementById("write").className = 'container-fluid popup-box show';
       var result = this.responseText;
       document.getElementById("transForm").reset();
-      alert("Successfully Added" + data.split('|||'))
+      alert("Successfully Added" + data.split('|||'));
+      read_action();
     }
   };
   xhttp.open("GET", "https://script.google.com/macros/s/AKfycbzp29Qzo_oLjAgi2UnhkRDl798lXFiU99Jy-aqXIuuE8NF0Ejlq/exec?row=" + data, true);
@@ -30,15 +31,40 @@ function read_action() {
       var result = "<br>"
       obj.forEach(function(item, index) {
         if (item.indexOf('Credit') < 0) {
-          result += '<p class="line-item">' + item + '</p>'
+          result += '<p class="line-item item">' + item + '</p>'
         } else {
-          result += '<p class="line-item red">' + item + '</p>'
+          result += '<p class="line-item item red">' + item + '</p>'
         }
+      })
+      document.getElementById("result").innerHTML = result;
+      var items = document.getElementsByClassName('item');
+      for(var i=0; i < items.length; i++) {
+        items[i].addEventListener('click', function(){read_more(this.innerHTML.split(':')[0])})
+      }
+    }
+  };
+  xhttp.open("GET", "https://script.google.com/macros/s/AKfycbwiLibhxusQjgb4yl_3ue0_wY_NojiSRQI1KZOu7HZXMapFO2k/exec", true);
+  xhttp.send();
+}
+
+function read_more(itemType){
+  var xhttp = new XMLHttpRequest();
+  document.getElementById("loading").className = 'container-fluid popup-box show';
+  document.getElementById("read").className = 'hidden';
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("loading").className = 'hidden';
+      document.getElementById("read").className = 'container-fluid popup-box show';
+      var obj = this.responseText.split('|');
+      var result = "<br>"
+      obj.forEach(function(item, index) {
+        splitted = item.split(',')
+        result += '<a href="' + splitted[splitted.length-1] +'" target="_blank"><p class="line-item item">' + splitted.slice(0,5) + '</p></a>'
       })
       document.getElementById("result").innerHTML = result;
     }
   };
-  xhttp.open("GET", "https://script.google.com/macros/s/AKfycbwiLibhxusQjgb4yl_3ue0_wY_NojiSRQI1KZOu7HZXMapFO2k/exec", true);
+  xhttp.open("GET", "https://script.google.com/macros/s/AKfycbxJciM0VIMhzUiWCEAaCsFPjIDHM8cQbp95Qus7cSsAb2C-b5W3/exec?cat=" + itemType, true);
   xhttp.send();
 }
 
